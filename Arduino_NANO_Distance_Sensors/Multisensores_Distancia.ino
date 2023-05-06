ab5e4294286717c45e7775d2d0b27a56d1180d6f
NakkaGS <gtnakata@gmail.com>
2023-05-06 13:04:17 +0200

    Update Multisensores_Distancia.ino

--- Arduino_NANO_Distance_Sensors/Multisensores_Distancia.ino
+++ Arduino_NANO_Distance_Sensors/Multisensores_Distancia.ino
@@ -5,53 +5,53 @@
 #include <Wire.h>
 #include <NewPing.h>
 
-#define SONAR_NUM_2     2 // Numero de Sensores do Arduino 2.
-#define ARDUINO_2       8 // Endereco do Arduino 2
+#define SONAR_NUM_2     2 // Number of Sensors
+#define ARDUINO_2       8 // Arduino Address
 
-#define MAX_DISTANCE 200 // Distancia maxima em cm.
-#define PING_INTERVAL 33 // Milisegundos entre os pings dos sensores
+#define MAX_DISTANCE 200 // Max Distance in cm.
+#define PING_INTERVAL 33 // Milisegunds between the pings
 
 unsigned long pingTimer[SONAR_NUM_2]; 
-unsigned int cm[SONAR_NUM_2];         // Aonde as distancias sao armazenadas
-uint8_t currentSensor = 0;          // verifica qual e o sensor ativo
+unsigned int cm[SONAR_NUM_2];         // Storage the Distances 
+uint8_t currentSensor = 0;          // Check which sensor is activated
  
-NewPing sonar[SONAR_NUM_2] = {     // Vetor de Sensores. Trigger pin / Echo Pin
-  NewPing(7, 8, MAX_DISTANCE), // Trigger / Echo / Maxima Distância
-  NewPing(5, 6, MAX_DISTANCE)  // Trigger / Echo / Maxima Distância
+NewPing sonar[SONAR_NUM_2] = {  
+  NewPing(7, 8, MAX_DISTANCE), // Trigger / Echo / Max Distance
+  NewPing(5, 6, MAX_DISTANCE)  // Trigger / Echo / Max Distance
 };
 
 
 void setup() {
-  Serial.begin(115200); // 
-  pingTimer[0] = millis() + 75;           // O primeiro ping começa em 75ms, dando um tempo para o Arduino inicializar
-  for (uint8_t i = 1; i < SONAR_NUM_2; i++) // Seta o tempo de inicio de cada sensore
+  Serial.begin(115200);
+  pingTimer[0] = millis() + 75;
+  for (uint8_t i = 1; i < SONAR_NUM_2; i++)
     pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
   
-  Wire.begin(ARDUINO_2);                // inicia o i2c com o endereco ARDUINO_2
-  Wire.onRequest(requestEvent); // caso seja solicitado este slave, ele vai para a funcao que foi atribuido
+  Wire.begin(ARDUINO_2);                // Start I2C with the address
+  Wire.onRequest(requestEvent); // if the MASTER request data, it will do the requestEvent
   
 }
 
 void loop() {
-  for (uint8_t i = 0; i < SONAR_NUM_2; i++) { // Da um loop em todos os sensores
+  for (uint8_t i = 0; i < SONAR_NUM_2; i++) { // Loop in the Sensors
     if (millis() >= pingTimer[i]) {         
-      pingTimer[i] += PING_INTERVAL * SONAR_NUM_2;  // seta o proximo ttempo que este sensor ira pingar
-      if (i == 0 && currentSensor == SONAR_NUM_2 - 1) oneSensorCycle(); // Ciclo completo, faca alguma coisa com os resultados
-      sonar[currentSensor].timer_stop();          // Para seguranca
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM_2;  
+      if (i == 0 && currentSensor == SONAR_NUM_2 - 1) oneSensorCycle(); // After end cycle, it does something with the data
+      sonar[currentSensor].timer_stop();          // For safety
       currentSensor = i;                         
-      cm[currentSensor] = 0;                      // Faz a distancia ser zero caso não tenho ping para este sensor
-      sonar[currentSensor].ping_timer(echoCheck); // Faz o ping
+      cm[currentSensor] = 0;                      // Distance is zero when is not detecting anything
+      sonar[currentSensor].ping_timer(echoCheck); // Ping
     }
   }
-  //Adicione aqui o que você quer caso queira adicionar algo no programa que nao tenha haver com o sensor de distancia
+  
 }
 
-void echoCheck() { // Se receber ping, seta a distancia do sensor no vetor
+void echoCheck() {  // get the ping of the sensor
   if (sonar[currentSensor].check_timer())
     cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
 }
 
-void oneSensorCycle() { // Ciclo completo, faca alguma coisa com os resultados
+void oneSensorCycle() { // After end cycle, it does something with the data
   for (uint8_t i = 0; i < SONAR_NUM_2; i++) {
     Serial.print(i);
     Serial.print("=");
@@ -63,13 +63,13 @@ void oneSensorCycle() { // Ciclo completo, faca alguma coisa com os resultados
 
 void requestEvent(){
   
-  uint8_t data[SONAR_NUM_2];  //vetor para envio de dados para o MASTER
+  uint8_t data[SONAR_NUM_2];  //data to be sent to MASTER
   for(int i = 0; i < SONAR_NUM_2; i++)
   {
     data[i] = cm[i];
   }
   
-  Wire.write(data,SONAR_NUM_2); // vetor, numero de bytes para enviar
+  Wire.write(data,SONAR_NUM_2); // vector, bytes numbers to send
 } 
 
 // ---------------------------------------------------------------------------
